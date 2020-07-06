2449817cd95e1aa635db5596cad8c96eb8ca5eb3
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2020-07-06 13:51:11 +0100

    Add base solution and recommended practices

---
+++ Solutions/Vellum.Cli/Program.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Program.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Vellum.Cli
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class Program
+    {
+        public static Task<int> Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            return Task.FromResult(0);
+        }
+    }
+}
