b462998b4e51e2afb9bfe6ff07eedc0194340dac
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2020-07-21 23:23:39 +0100

    Add uninstallation

--- Solutions/Vellum.Cli/Vellum/Cli/Commands/Templates/TemplatePackageUninstallerHandler.cs
+++ Solutions/Vellum.Cli/Vellum/Cli/Commands/Templates/TemplatePackageUninstallerHandler.cs
@@ -6,19 +6,46 @@ namespace Vellum.Cli.Commands.Templates
 {
     using System.CommandLine;
     using System.CommandLine.Invocation;
+    using System.CommandLine.IO;
     using System.Threading.Tasks;
     using Vellum.Cli.Abstractions;
     using Vellum.Cli.Abstractions.Environment;
+    using Vellum.Cli.Abstractions.Templates;
+    using Vellum.Cli.Templates;
 
     public static class TemplatePackageUninstallerHandler
     {
-        public static Task<int> ExecuteAsync(
+        public static async Task<int> ExecuteAsync(
             TemplateOptions options,
             IConsole console,
             IAppEnvironment appEnvironment,
             InvocationContext context = null)
         {
-            return Task.FromResult(ReturnCodes.Ok);
+            console.Out.WriteLine($"Uninstalling template package '{options.PackageId}'");
+
+            var templateSettingsManager = new TemplateSettingsManager(appEnvironment);
+            TemplatesSettings currentSettings = templateSettingsManager.LoadSettings() ?? new TemplatesSettings();
+
+            var packageManager = new NuGetTemplatePackageManager(appEnvironment);
+
+            if (currentSettings.Packages.Exists(x => x.PackageId == options.PackageId))
+            {
+                TemplatePackage package = currentSettings.Packages.Find(x => x.Id == options.PackageId);
+
+                if (package != null)
+                {
+                    await packageManager.UnnstallAsync(package).ConfigureAwait(false);
+
+                    currentSettings.Packages.Remove(package);
+
+                    if (currentSettings.DefaultTemplate.PackageName == package.PackageId)
+                    {
+                        currentSettings.DefaultTemplate = new DefaultTemplate();
+                    }
+                }
+            }
+
+            return ReturnCodes.Ok;
         }
     }
 }
