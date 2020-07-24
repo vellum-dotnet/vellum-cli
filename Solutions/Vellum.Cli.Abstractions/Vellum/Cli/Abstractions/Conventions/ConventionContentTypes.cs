8ee53ed1afd45bd296a0fbd8e9301994c04bb445
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2020-07-24 09:34:05 +0100

    allow new blog post to be create from the blog post template

---
+++ Solutions/Vellum.Cli.Abstractions/Vellum/Cli/Abstractions/Conventions/ConventionContentTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace Vellum.Cli.Abstractions.Conventions
+{
+    public class ContentTypes
+    {
+        public const string FilePaths = "application/vnd.vellum.conventions.file-path";
+    }
+}
