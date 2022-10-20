a3580d112b07ee01dbc8a92199b1e86440fb2414
dependjinbot <dependjinbot@endjin.com>
2022-10-20 03:04:26 +0000

    Updated scripted build

--- build.ps1
+++ build.ps1
@@ -71,7 +71,7 @@ param (
     [string] $BuildModulePath,
 
     [Parameter()]
-    [version] $BuildModuleVersion = "0.2.15",
+    [version] $BuildModuleVersion = "0.2.16",
 
     [Parameter()]
     [version] $InvokeBuildModuleVersion = "5.7.1"
