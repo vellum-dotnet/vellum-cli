2b2e31cd004449321d0d9e5632fb427e1947b2dc
Howard van Rooijen <howard.vanrooijen@endjin.com>
2022-12-17 09:21:05 +0000

    Update packages and plugin handling. (#187)

--- Solutions/Vellum.Cli/Vellum/Cli/Plugins/CommandPluginHost.cs
+++ Solutions/Vellum.Cli/Vellum/Cli/Plugins/CommandPluginHost.cs
@@ -4,60 +4,76 @@
 
 namespace Vellum.Cli.Plugins
 {
-    using System;
-    using System.Collections.Generic;
-    using System.CommandLine;
-    using System.Linq;
-    using McMaster.NETCore.Plugins;
-    using NDepend.Path;
-    using Vellum.Cli.Abstractions.Commands;
+  using System;
+  using System.Collections.Generic;
+  using System.CommandLine;
+  using System.Linq;
 
-    public class CommandPluginHost : ICommandPluginHost
+  using McMaster.NETCore.Plugins;
+
+  using NDepend.Path;
+
+  using Vellum.Cli.Abstractions.Commands;
+
+  public class CommandPluginHost : ICommandPluginHost
+  {
+    public IEnumerable<Command> Discover(IEnumerable<IAbsoluteDirectoryPath> pluginPaths)
     {
-        public IEnumerable<Command> Discover(IEnumerable<IAbsoluteDirectoryPath> pluginPaths)
+      var loaders = new List<PluginLoader>();
+      var assemblies = pluginPaths.SelectMany(dir => dir.ChildrenFilesPath.Where(x => x.FileExtension == ".dll"))
+                                                       .DistinctBy(x => x.FileName)
+                                                       .ToList();
+
+      foreach (IAbsoluteFilePath assembly in assemblies)
+      {
+        // Enable Hot Reload so that we can delete plugins, otherwise you get an access denied exception
+        var pluginLoader = PluginLoader.CreateFromAssemblyFile(
+            assembly.FileInfo.FullName,
+            sharedTypes: new[] { typeof(ICommandPlugin) },
+            config => config.EnableHotReload = true);
+
+        loaders.Add(pluginLoader);
+      }
+
+      List<Command> loadedCommands = new();
+
+      foreach (PluginLoader loader in loaders)
+      {
+        try
         {
-            var loaders = new List<PluginLoader>();
-            var assemblies = pluginPaths.SelectMany(dir => dir.ChildrenFilesPath.Where(x => x.FileExtension == ".dll")).DistinctBy(x => x.FileName).ToList();
+          foreach (Type pluginType in loader
+                       .LoadDefaultAssembly()
+                       .GetTypes()
+                       .Where(t => typeof(ICommandPlugin).IsAssignableFrom(t) && !t.IsAbstract))
+          {
+            Command command = null;
 
-            foreach (IAbsoluteFilePath assembly in assemblies)
+            try
             {
-                // Enable Hot Reload so that we can delete plugins, otherwise you get an access denied exception
-                var pluginLoader = PluginLoader.CreateFromAssemblyFile(
-                    assembly.FileInfo.FullName,
-                    sharedTypes: new[] { typeof(ICommandPlugin) },
-                    config => config.EnableHotReload = true);
-
-                loaders.Add(pluginLoader);
+              // This assumes the implementation of IPlugin has a parameter-less constructor
+              var plugin = Activator.CreateInstance(pluginType) as ICommandPlugin;
+              command = plugin?.Command();
+            }
+            catch (Exception exception)
+            {
+              Console.WriteLine($"Cannot Load {pluginType.FullName}");
+              Console.WriteLine($"You may need to manually delete the plugin or run the plugins clear command.");
+              Console.WriteLine(exception.Message);
             }
 
-            foreach (PluginLoader loader in loaders)
+            if (command != null)
             {
-                foreach (Type pluginType in loader
-                             .LoadDefaultAssembly()
-                             .GetTypes()
-                             .Where(t => typeof(ICommandPlugin).IsAssignableFrom(t) && !t.IsAbstract))
-                {
-                    Command command = null;
-
-                    try
-                    {
-                        // This assumes the implementation of IPlugin has a parameter-less constructor
-                        var plugin = Activator.CreateInstance(pluginType) as ICommandPlugin;
-                        command = plugin?.Command();
-                    }
-                    catch (Exception exception)
-                    {
-                        Console.WriteLine($"Cannot Load {pluginType.FullName}");
-                        Console.WriteLine("You may need to manually delete the plugin or run the plugins clear command.");
-                        Console.WriteLine(exception.Message);
-                    }
-
-                    if (command != null)
-                    {
-                        yield return command;
-                    }
-                }
+              loadedCommands.Add(command);
             }
+          }
         }
+        catch (Exception exception)
+        {
+          Console.WriteLine(exception.Message);
+        }
+      }
+
+      return loadedCommands;
     }
+  }
 }
